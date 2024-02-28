5edcaff67647a4dc8469199d524c216d1b579342
pingjui <pingjuiliao@gmail.com>
2024-02-27 18:01:14 -0800

    add more tests

---
+++ to-include/include-tree/include/MyAPI/util/log.h
@@ -0,0 +1,8 @@
+#ifndef LOG_H
+#define LOG_H
+#define log(x) do { \
+  std::cout << x << std::endl;  \
+} while(0);
+
+
+#endif // LOG_H
---
+++ to-include/include-tree/src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "MyAPI/util/log.h"
+
+int main(int argc, char** argv) {
+  log("helloworld");
+  return 0;
+}
+
